6c59aa2b6df45e93695c6b6b8757b7e97aae3e18
icarusicarus <bluemonster@pusan.ac.kr>
2021-09-08 12:21:48 +0900

    Add PyQt setting

---
+++ PathFind.py
@@ -0,0 +1,18 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+class MyApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('CAMEL Path Finder')
+        self.move(300, 300)
+        self.resize(1000, 700)
+        self.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
