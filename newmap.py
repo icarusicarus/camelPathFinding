beb0309c964a545dc169ff18f82c7fb1f5ff6f7e
icarusicarus <bluemonster@pusan.ac.kr>
2021-09-15 01:41:15 +0900

    Change astar to inclass method

--- newmap.py
+++ newmap.py
@@ -13,135 +13,6 @@ length = imagewidth//horizontalStepCount
 Color = {0: "none", 1: "grey", 2: "green", 3: "red"}  # 열린목록 닫힌목록 길 등은 나중에 추가
 
 
-class Node:
-    def __init__(self, parent=None, position=None):
-        self.parent = parent
-        self.position = position
-
-        self.g = 0
-        self.h = 0
-        self.f = 0
-
-    def __eq__(self, other):
-        return self.position == other.position
-
-
-def heuristic(node, goal, D=1, D2=2 ** 0.5):  # Diagonal Distance
-    dx = abs(node.position[0] - goal.position[0])
-    dy = abs(node.position[1] - goal.position[1])
-    return D * (dx + dy) + (D2 - 2 * D) * min(dx, dy)
-
-
-def aStar(maze, start, end):
-    print('start end', start, end)
-    # startNode와 endNode 초기화
-    startNode = Node(None, start)
-    endNode = Node(None, end)
-    print('Nodes', startNode, endNode)
-
-    # openList, closedList 초기화
-    openList = []
-    closedList = []
-
-    # openList에 시작 노드 추가
-    openList.append(startNode)
-    print('init openList', openList)
-
-    # endNode를 찾을 때까지 실행
-    while openList:
-        print(
-            '============================================================================')
-        print('openList', openList)
-        # 현재 노드 지정
-        currentNode = openList[0]
-        currentIdx = 0
-
-        # 이미 같은 노드가 openList에 있고, f 값이 더 크면
-        # currentNode를 openList안에 있는 값으로 교체
-        for index, item in enumerate(openList):
-            if item.f < currentNode.f:
-                currentNode = item
-                currentIdx = index
-        #print('currentNode', currentNode)
-        #print('currentIdx', currentIdx)
-        print('currentNode', currentNode)
-        print('currentNode.position', currentNode.position)
-        # openList에서 제거하고 closedList에 추가
-        openList.pop(currentIdx)
-        closedList.append(currentNode)
-        #print('closedList', closedList)
-        #print('currentNode.position', currentNode.position)
-        # 현재 노드가 목적지면 current.position 추가하고
-        # current의 부모로 이동
-        if currentNode.position == endNode.position:
-            path = []
-            current = currentNode
-            while current is not None:
-                # maze 길을 표시하려면 주석 해제
-                # x, y = current.position
-                # maze[x][y] = 7
-                path.append(current.position)
-                current = current.parent
-            print('!!!!!!!!!!!!!!!!!!!!!!!!!path!!!!!!!!!!!!!!!!!!!!!!!!!', path)
-            return path[::-1]  # reverse
-
-        children = []
-        # 인접한 xy좌표 전부
-        for newPosition in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]:
-
-            # 노드 위치 업데이트
-            nodePosition = (
-                currentNode.position[0] + newPosition[0],  # X
-                currentNode.position[1] + newPosition[1])  # Y
-
-            # 미로 maze index 범위 안에 있어야함
-            within_range_criteria = [
-                nodePosition[0] > (len(maze) - 1),
-                nodePosition[0] < 0,
-                nodePosition[1] > (len(maze[len(maze) - 1]) - 1),
-                nodePosition[1] < 0,
-            ]
-
-            if any(within_range_criteria):  # 하나라도 true면 범위 밖임
-                continue
-
-            # 장애물이 있으면 다른 위치 불러오기
-            if maze[nodePosition[0]][nodePosition[1]] == 1:
-                continue
-
-            new_node = Node(currentNode, nodePosition)
-            #print('child new', new_node)
-            children.append(new_node)
-
-        # 자식들 모두 loop
-        for child in children:
-
-            # 자식이 closedList에 있으면 continue
-            if child in closedList:
-                continue
-
-            # if child in openList:
-            #     continue
-
-            # f, g, h값 업데이트
-            child.g = currentNode.g + 1
-            child.h = ((child.position[0] - endNode.position[0]) **
-                       2) + ((child.position[1] - endNode.position[1]) ** 2)
-            # child.h = heuristic(child, endNode) 다른 휴리스틱
-            # print("position:", child.position) 거리 추정 값 보기
-            # print("from child to goal:", child.h)
-
-            child.f = child.g + child.h
-            #print('child', child.position, child.f)
-
-            # 자식이 openList에 있으고, g값이 더 크면 continue
-            if len([openNode for openNode in openList
-                    if child == openNode and child.g > openNode.g]) > 0:
-                continue
-
-            openList.append(child)
-
-
 class AStarPathFinding:
     # ------------------------------------------------------------------
     # Initialization Functions:
@@ -239,140 +110,109 @@ class AStarPathFinding:
     def BTgoal(self):
         self.modenumber = 3
 
-    # def aStar(self):
-    #     class Node:
-    #         def __init__(self, parent=None, position=None):
-    #             self.parent = parent
-    #             self.position = position
-
-    #             self.g = 0
-    #             self.h = 0
-    #             self.f = 0
-
-    #         def __eq__(self, other):
-    #             return self.position == other.position
-    #     maze = self.board
-    #     print('=======maze=======\n', maze)
-    #     start = (np.where(self.board == 2)[0].tolist()[0],
-    #              np.where(self.board == 2)[1].tolist()[0])
-    #     end = (np.where(self.board == 3)[0].tolist()[0],
-    #            np.where(self.board == 3)[1].tolist()[0])
-    #     print('start end', start, end)
-    #     # startNode와 endNode 초기화
-    #     startNode = Node(None, start)
-    #     endNode = Node(None, end)
-    #     print('Nodes', startNode, endNode)
-
-    #     # openList, closedList 초기화
-    #     openList = []
-    #     closedList = []
-
-    #     # openList에 시작 노드 추가
-    #     openList.append(startNode)
-    #     print('init openList', openList)
-
-    #     # endNode를 찾을 때까지 실행
-    #     while openList:
-    #         print(
-    #             '============================================================================')
-    #         #print('openList', openList)
-    #         # 현재 노드 지정
-    #         currentNode = openList[0]
-    #         currentIdx = 0
-
-    #         # 이미 같은 노드가 openList에 있고, f 값이 더 크면
-    #         # currentNode를 openList안에 있는 값으로 교체
-    #         for index, item in enumerate(openList):
-    #             if item.f < currentNode.f:
-    #                 currentNode = item
-    #                 currentIdx = index
-    #         # openList에서 제거하고 closedList에 추가
-    #         openList.pop(currentIdx)
-    #         closedList.append(currentNode)
-    #         #print('closedList', closedList)
-    #         print('currentNode', currentNode)
-    #         print('currentNode.position', currentNode.position)
-    #         self.canvas.create_rectangle(
-    #             (currentNode.position[1])*length, (currentNode.position[0])*length, (currentNode.position[1]+1)*length, (currentNode.position[0]+1)*length, tag="sth", fill="yellow")
-    #         # 보드에 createRec (초록색)
-
-    #         # 현재 노드가 목적지면 current.position 추가하고
-    #         # current의 부모로 이동
-    #         if currentNode == endNode:
-    #             path = []
-    #             print('!!!!!!!!!!!!!!!!!!!!!!!!!path!!!!!!!!!!!!!!!!!!!!!!!!!', path)
-    #             current = currentNode
-    #             while current is not None:
-    #                 # maze 길을 표시하려면 주석 해제
-    #                 # x, y = current.position
-    #                 # maze[x][y] = 7
-    #                 path.append(current.position)
-    #                 current = current.parent
-    #             return path[::-1]  # reverse
-
-    #         children = []
-    #         # 인접한 xy좌표 전부
-    #         for newPosition in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]:
-
-    #             # 노드 위치 업데이트
-    #             nodePosition = (
-    #                 currentNode.position[0] + newPosition[0],  # X
-    #                 currentNode.position[1] + newPosition[1])  # Y
-
-    #             # 미로 maze index 범위 안에 있어야함
-    #             within_range_criteria = [
-    #                 nodePosition[0] > (len(maze) - 1),
-    #                 nodePosition[0] < 0,
-    #                 nodePosition[1] > (len(maze[len(maze) - 1]) - 1),
-    #                 nodePosition[1] < 0,
-    #             ]
-
-    #             if any(within_range_criteria):  # 하나라도 true면 범위 밖임
-    #                 continue
-
-    #             # 장애물이 있으면 다른 위치 불러오기
-    #             if maze[nodePosition[0]][nodePosition[1]] != 0:
-    #                 continue
-
-    #             new_node = Node(currentNode, nodePosition)
-    #             #print('child new', new_node)
-    #             children.append(new_node)
-
-    #         # 자식들 모두 loop
-    #         for child in children:
-    #             # 자식이 closedList에 있으면 continue
-    #             if child in closedList:
-    #                 continue
-
-    #             # f, g, h값 업데이트
-    #             child.g = currentNode.g + 1
-    #             child.h = ((child.position[0] - endNode.position[0]) **
-    #                        2) + ((child.position[1] - endNode.position[1]) ** 2)
-    #             # child.h = heuristic(child, endNode) 다른 휴리스틱
-    #             # print("position:", child.position) 거리 추정 값 보기
-    #             # print("from child to goal:", child.h)
-
-    #             child.f = child.g + child.h
-    #             #print('child', child.position, child.f)
-
-    #             # 자식이 openList에 있으고, g값이 더 크면 continue
-    #             if len([openNode for openNode in openList
-    #                     if child == openNode and child.g > openNode.g]) > 0:
-    #                 continue
-
-    #             openList.append(child)
-    #             self.drawRec(child.position)
-    #             # self.canvas.create_rectangle(
-    #             #     (child.position[1])*length, (child.position[0])*length, (child.position[1]+1)*length, (child.position[0]+1)*length, tag="sth", fill="blue")
-
-    #     #print('closedList\n', closedList)
-
     def BTastar(self):
+        class Node:
+            def __init__(self, parent=None, position=None):
+                self.parent = parent
+                self.position = position
+
+                self.g = 0
+                self.h = 0
+                self.f = 0
+
+            def __eq__(self, other):
+                return self.position == other.position
+
+        def aStar(maze, start, end):
+            startNode = Node(None, start)
+            endNode = Node(None, end)
+
+            # openList, closedList 초기화
+            openList = []
+            closedList = []
+
+            # openList에 시작 노드 추가
+            openList.append(startNode)
+
+            # endNode를 찾을 때까지 실행
+            while openList:
+                # 현재 노드 지정
+                currentNode = openList[0]
+                currentIdx = 0
+
+                # 이미 같은 노드가 openList에 있고, f 값이 더 크면
+                # currentNode를 openList안에 있는 값으로 교체
+                for index, item in enumerate(openList):
+                    if item.f < currentNode.f:
+                        currentNode = item
+                        currentIdx = index
+                # openList에서 제거하고 closedList에 추가
+                openList.pop(currentIdx)
+                closedList.append(currentNode)
+                # 현재 노드가 목적지면 current.position 추가하고
+                # current의 부모로 이동
+                if currentNode.position == endNode.position:
+                    path = []
+                    current = currentNode
+                    while current is not None:
+                        path.append(current.position)
+                        current = current.parent
+                    print(
+                        '!!!!!!!!!!!!!!!!!!!!!!!!!path!!!!!!!!!!!!!!!!!!!!!!!!!', path)
+                    return path[::-1]  # reverse
+
+                children = []
+                # 인접한 xy좌표 전부
+                for newPosition in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]:
+
+                    # 노드 위치 업데이트
+                    nodePosition = (
+                        currentNode.position[0] + newPosition[0],  # X
+                        currentNode.position[1] + newPosition[1])  # Y
+
+                    # 미로 maze index 범위 안에 있어야함
+                    within_range_criteria = [
+                        nodePosition[0] > (len(maze) - 1),
+                        nodePosition[0] < 0,
+                        nodePosition[1] > (len(maze[len(maze) - 1]) - 1),
+                        nodePosition[1] < 0,
+                    ]
+
+                    if any(within_range_criteria):  # 하나라도 true면 범위 밖임
+                        continue
+
+                    # 장애물이 있으면 다른 위치 불러오기
+                    if maze[nodePosition[0]][nodePosition[1]] == 1:
+                        continue
+
+                    new_node = Node(currentNode, nodePosition)
+                    children.append(new_node)
+
+                # 자식들 모두 loop
+                for child in children:
+
+                    # 자식이 closedList에 있으면 continue
+                    if child in closedList:
+                        continue
+
+                    # f, g, h값 업데이트
+                    child.g = currentNode.g + 1
+                    child.h = ((child.position[0] - endNode.position[0]) **
+                               2) + ((child.position[1] - endNode.position[1]) ** 2)
+                    child.f = child.g + child.h
+
+                    # 자식이 openList에 있으고, g값이 더 크면 continue
+                    if len([openNode for openNode in openList
+                            if child == openNode and child.g > openNode.g]) > 0:
+                        continue
+
+                    openList.append(child)
+
         start = (np.where(self.board == 2)[0].tolist()[0],
                  np.where(self.board == 2)[1].tolist()[0])
         end = (np.where(self.board == 3)[0].tolist()[0],
                np.where(self.board == 3)[1].tolist()[0])
-        aStar(self.board.tolist(), start, end)
+        aStar(self.board, start, end)
 
     def BTsave(self):
         self.newWindow = Toplevel(self.window)
